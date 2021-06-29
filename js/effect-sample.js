46b6e0890901d2a2921e86427b1d506a07c7faca
komiyaakie <86590618+komiyaakie@users.noreply.github.com>
2021-06-29 23:34:24 +0900

    Add files via upload

---
+++ js/effect-sample.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+  　loop: true,
+    navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+    },
+    centeredSlides: true,
+    slidesPerView: 1.2
+});
