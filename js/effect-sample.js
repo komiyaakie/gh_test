ce1d787d5e0d3b200d63dc05d7ef39872a50d4b3
komiyaakie <86590618+komiyaakie@users.noreply.github.com>
2021-07-03 16:43:02 +0900

    Add files via upload

--- js/effect-sample.js
+++ js/effect-sample.js
@@ -1,9 +1,21 @@
 var swiper = new Swiper('.swiper-container', {
   　loop: true,
     navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    paginationClickable: true,
     centeredSlides: true,
-    slidesPerView: 1.2
+    speed: 2000,
+    autoplay: {
+      delay: 5000,
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true
+    }
 });
